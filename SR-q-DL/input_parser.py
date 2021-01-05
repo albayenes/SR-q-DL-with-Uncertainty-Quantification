c4b636763bc95000dcc2cf1ed3d9eb1ab916abc5
Enes Albay <albayenes@gmail.com>
2021-01-05 23:56:13 +0300

    set device gpu

--- SR-q-DL/input_parser.py
+++ SR-q-DL/input_parser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jan 13 16:46:09 2019
--- SR-q-DL/models.py
+++ SR-q-DL/models.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 12 11:29:01 2019
